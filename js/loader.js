fb4d8edf5a0f862476d8791678c8e602218d6feb
GhostDDog <nanartwork@gmail.com>
2023-05-01 13:41:01 +0200

    loader maj 2

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
-    document.querySelector('.container-loader').classList.add('hide');
+    document.querySelector('.container-loader').style.animationPlayState = 'running';
     document.querySelector('.container-loader').style.display = 'none';
     });
 
